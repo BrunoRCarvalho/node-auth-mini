af04df64234aafa5d3eb8493781ecb276656dd76
Bruno Carvalho <dipsnatchy26@gmail.com>
2018-01-04 12:13:58 -0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const session = require('express-session');
 
+const passport = require('passport')
+const strategy = require('./strategy')
+
 const app = express();
+
+app.use( session({
+  secret: 'sup dude',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  return done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+  return done(null, user)
+})
+
+app.get('/login', passport.authenticate('auth0', {
+  successRedirect: '/me',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/me', function(req, res) {
+  if(req.user) {
+    return res.json(req.user)
+  } else {
+    return res.redirect('/login')
+  }
+})
+
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
